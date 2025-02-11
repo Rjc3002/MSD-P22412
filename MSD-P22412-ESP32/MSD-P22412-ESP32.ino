048cac457dbe3f99408ea11b39a5064484c0487c
Rjc3002 <rjc3002@rit.edu>
2025-02-10 21:46:53 -0500

    Add project files.

---
+++ MSD-P22412-ESP32/MSD-P22412-ESP32.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MSD_P22412_ESP32.ino
+ Created:	2/10/2025 9:29:49 PM
+ Author:	rylan
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
